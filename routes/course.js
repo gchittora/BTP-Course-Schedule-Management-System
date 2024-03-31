9c034f32d0eb69eace027a1812a8a28c03c8fa54
JOSH1412 <137147129+JOSH1412@users.noreply.github.com>
2024-03-31 11:48:37 +0530

    Update course.js

--- routes/course.js
+++ routes/course.js
@@ -5,11 +5,21 @@ const Schema = mongoose.Schema;
 const courseSchema = new Schema({
     name: { type: String, required: true },
     year: { type: String, required: true },
-    semester: { type: String,  },
-    numberOfStudents: { type: Number, required: true },
-    department: { type: Schema.Types.ObjectId, ref: 'Department', required: true },
-    instructor: { type: Schema.Types.ObjectId, ref: 'Professor' },
-    methodOfDelivery: { type: String, enum: ['horizontal', 'vertical'] }
+    semester: { type: String },
+    courseCode: { type: String , default: "null" },
+    numberOfStudents: {
+        CSE: { type: Number, default: 0 },
+        CCE: { type: Number, default: 0 },
+        ECE: { type: Number, default: 0 },
+        CSE_DD: { type: Number, default: 0 },
+        ECE_DD: { type: Number, default: 0 },
+        MME: { type: Number, default: 0 }
+    },
+    department: { type: Schema.Types.ObjectId, ref:'Department'}, 
+    courseType: { type: String, enum: ['Core','Elective','Other']},
+    sharingType: { type: String},
+    credits: { type: Number },
+    professors: [{ type: Schema.Types.ObjectId, ref: 'Professor' }]
 });
 
 // Create and export the Course model
