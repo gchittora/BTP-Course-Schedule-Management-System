f4c2e5d9db8535fe083bbabdb6a541a3852d01f9
gchittora <96302950+gchittora@users.noreply.github.com>
2024-05-28 16:44:02 +0530

    Update course.js

--- routes/course.js
+++ routes/course.js
@@ -1,28 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Define the course schema
 const courseSchema = new Schema({
-    name: { type: String, required: true },
-    year: { type: String, required: true },
-    semester: { type: String },
-    courseCode: { type: String , default: "null" },
-    numberOfStudents: {
-        CSE: { type: Number, default: 0 },
-        CCE: { type: Number, default: 0 },
-        ECE: { type: Number, default: 0 },
-        CSE_DD: { type: Number, default: 0 },
-        ECE_DD: { type: Number, default: 0 },
-        MME: { type: Number, default: 0 }
-    },
-    department: { type: Schema.Types.ObjectId, ref:'Department'}, 
-    courseType: { type: String, enum: ['Core','Elective','Other']},
-    sharingType: { type: String},
-    credits: { type: Number },
-    professors: [{ type: Schema.Types.ObjectId, ref: 'Professor' }]
+  name: { type: String, required: true, unique: true },
+  year: { type: String, required: true },
+  semester: { type: String },
+  courseCode: { type: String, default: "null" },
+  program:{ type: String, default: "B.Tech." },
+  numberOfStudents: {
+    CSE: { type: Number, default: 0 },
+    CCE: { type: Number, default: 0 },
+    ECE: { type: Number, default: 0 },
+    CSE_DD: { type: Number, default: 0 },
+    ECE_DD: { type: Number, default: 0 },
+    MME: { type: Number, default: 0 },
+    MScPHY:{type:Number,default:0},
+    MScMTH:{type:Number,default:0}
+  },
+  department: { type: Schema.Types.ObjectId, ref: "Department" },
+  courseType: { type: String },
+  sharingType: { type: String },
+  credits: { type: Number },
+  professors: [{ type: Schema.Types.ObjectId, ref: "Professor" }],
+  group: { type: String, default: "NA" },
 });
 
 // Create and export the Course model
-const Course = mongoose.model('Course', courseSchema);
+const Course = mongoose.model("Course", courseSchema);
 
 module.exports = Course;
